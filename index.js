0f311c4392140e9f60bc305cc9cf9ad8d04662d5
Avalynn <avalynndev@gmail.com>
2024-04-08 18:33:05 +0530

    remove music commands
    distube not supported cuz itz bad..
    
    Co-Authored-By: MinecoolYT <72187396+MinecoolYT@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { SoundCloudPlugin } = require("@distube/soundcloud");
 require("dotenv").config();
 
 // Env Defines //
-spotify_id = process.env.spotify_id;
-spotify_secret = process.env.spotify_secret;
 databaseuri = process.env.DatabaseURL;
 token = process.env.TOKEN;
 
@@ -20,21 +18,6 @@ const client = new Client({
 
 const { loadEvents } = require("./Handlers/eventHandler");
 
-let spotifyoptions = {
-  parallel: true,
-  emitEventsAfterFetching: true,
-  api: {
-    clientId: spotify_id,
-    clientSecret: spotify_secret,
-  },
-};
-client.distube = new DisTube(client, {
-  emitNewSongOnly: true,
-  leaveOnFinish: true,
-  emitAddSongWhenCreatingQueue: false,
-  plugins: [new SpotifyPlugin(spotifyoptions), new SoundCloudPlugin()],
-});
-
 client.events = new Collection();
 client.subCommands = new Collection();
 client.commands = new Collection();
@@ -43,95 +26,5 @@ const { connect } = require("mongoose");
 connect(databaseuri, {}).then(() => console.log("Database ✅"));
 
 loadEvents(client);
-
-client.distube
-  .on("playSong", (queue, song) =>
-    queue.textChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(
-            `📀 | Playing: [${song.name}](${song.url}) - \`${song.formattedDuration}\``
-          )
-          .setFooter({
-            text: `Requested by ${song.user.username} | Bot by AvalynnDev`,
-            iconURL: `${song.user.displayAvatarURL()}`,
-          })
-          .setTimestamp()
-          .setColor("Random"),
-      ],
-    })
-  )
-  .on("addSong", (queue, song) =>
-    queue.textChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(
-            `📀 | Added [${song.name}](${song.url}) - \`${song.formattedDuration}\``
-          )
-          .setFooter({
-            text: `Requested by ${song.user.username} | Bot by AvalynnDev`,
-            iconURL: `${song.user.displayAvatarURL()}`,
-          })
-          .setTimestamp()
-          .setColor("Random"),
-      ],
-    })
-  )
-  .on("playList", (queue, playlist, song) =>
-    queue.textChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(
-            `📀 | Play [${playlist.name}](${playlist.url}) playlist (${playlist.songs.length} songs).\nNow playing [${song.name}](${song.url}) - \`${song.formattedDuration}\``
-          )
-          .setFooter({
-            text: `Requested by ${song.user.username} | Bot by AvalynnDev`,
-            iconURL: `${song.user.displayAvatarURL()}`,
-          })
-          .setTimestamp()
-          .setColor("Random"),
-      ],
-    })
-  )
-  .on("addList", (queue, playlist) =>
-    queue.textChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(
-            `📀 | Added [${playlist.name}](${playlist.url}) playlist (${playlist.songs.length} songs) to queue.`
-          )
-          .setColor("Random"),
-      ],
-    })
-  )
-  .on("empty", (queue) =>
-    queue.textChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(
-            `📀 | Looks like everyone left me alone in the Voice Channel so I am leaving Voice Channel too.`
-          )
-          .setColor("Random"),
-      ],
-    })
-  )
-  .on(`error`, (channel, e) => {
-    channel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(`:x: | An error encountered: ${e}`)
-          .setColor("Random"),
-      ],
-    });
-  })
-  .on("finish", (queue) =>
-    queue.textChannel.send({
-      embeds: [
-        new EmbedBuilder()
-          .setDescription(`📀 | Music Queue has just ended`)
-          .setColor("Random"),
-      ],
-    })
-  );
 
 client.login(token);
